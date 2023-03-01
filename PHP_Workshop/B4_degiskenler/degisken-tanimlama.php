0bfc10ca292cd1fcddfe37cfdc2976a13e43caa7
Elif Beyza Tok <102792446+elifbeyzatok00@users.noreply.github.com>
2023-03-01 13:41:50 +0300

    Add B4_degiskenler

---
+++ PHP_Workshop/B4_degiskenler/degisken-tanimlama.php
@@ -0,0 +1,27 @@
+<?php
+
+   $sayi1 = 10;
+   $sayi2 = 20;
+
+   // $1sayi => yanlış
+   /*
+   değişkenler sayısal karakter ile başlayamaz
+   */
+
+   $sayi3 = 30;
+   // $sayi 3 = 30;
+   // değişkenler isimleri arasında boşluk karakteri olamaz
+
+   $isim = "Sadık";
+   $soyad = 'Turan';
+
+   $isiM = "Çınar";
+   //Büyük küçük harf duyarlılığı var
+   //degisken oluştururken türkçe karakter kullanmayınız
+
+   //echo $sayi1." ".$sayi2;
+
+   echo $isim." ".$soyad;
+   echo "<br>";
+   echo $isiM." ".$soyad;
+?>
---
+++ PHP_Workshop/B4_degiskenler/sabitler.php
@@ -0,0 +1,15 @@
+<?php
+    #Php Sabitler
+    #Sabitler veri saklamak için kullanılır ancak atanan değer değiştirilemez
+
+    define("baglanti", "mssql");
+    define("username", "sadikturan");
+    const password = "1234";
+
+    echo baglanti."<br>";  //mssql
+    echo username."<br>";  //sadikturan
+    echo password."<br>";  //1234
+
+
+
+?>
