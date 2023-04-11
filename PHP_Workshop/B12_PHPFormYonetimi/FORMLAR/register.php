3c3018c36c285e1c2fafea83997b1fbb6e50c071
Elif Beyza Tok <102792446+elifbeyzatok00@users.noreply.github.com>
2023-04-11 17:17:04 +0300

    Add files via upload

--- PHP_Workshop/B12_PHPFormYonetimi/FORMLAR/register.php
+++ PHP_Workshop/B12_PHPFormYonetimi/FORMLAR/register.php
@@ -1,21 +1,92 @@
 <!-- en altta form ile gönderdiğimiz bilgileri "aynı sayfa içerisinde" yukarıda yakalayalım -->
     
 <?php 
+    $adsoyad =  $email =  $parola = $parola2 = $sehir = $cinsiyet = $tanitim = "";
+    $hobiler = [];
+
+    $adsoyadErr =  $emailErr =  $parolaErr = $parola2Err = $sehirErr = $cinsiyetErr = $tanitimErr = $hobilerErr = "";
+
+    function control_input($data){
+        $data = trim($data); // trim() içine yazılan karakterin başındaki ve sonundaki boşluğu yok eder
+        $data = stripcslashes($data); // sql injection için önlem (ters tırnakların önüne slash koyar)
+        $data = htmlspecialchars($data); // gönderilen karakterlerin html entity karşılıklarını yazar. Yani kullanıcının script göndermesini engeller
+        return $data;
+    }
+    /*
+     $data = htmlspecialchars($data); // gönderilen karakterlerin html entity karşılıklarını yazar. Yani kullanıcının script göndermesini engeller
+        
+     örneğin adınız kısmına <script>alert('merhaba')</script> yazalım.
+     htmlspecialchars() kullanmazsam kaynak kodun çıktısı:
+    <script>alert('mesaj')</script> (kayıt ol a bastıktan sonra alarm da verir) -> bunu istemiyoruz
+     htmlspecialchars() sayesinde kaynak kodun çıktısı:
+        &lt;script&gt;alert(&#039;mesaj&#039;)&lt;/script&gt;
+        &lt(less than), < işaretinin entity karşılığı
+    */
+
+
     //sayfa ilk yüklendiğinde formdan hiçbir şey gönderilmediği için en yukarıda php kısmı hata gösterir
     //bunun önüne nasıl geçmek için if bloğu ekleyelim
     //if($_SERVER["REQUEST_METHOD"]) ile sayfanın çağırıldığı andan itibarenki method tipine ulaşılır
     //if($_SERVER["REQUEST_METHOD"] == "POST"), if bloğu sayfa sadece POST methodu ile çağırıldığında çalışır
     //sayfa en başta GET ile çağırıldığı için de if bloğu formdan POST ile bilgi alana kadar çalışmaz ve artık hata almayız
-
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $adsoyad = $_POST['adsoyad'];
-    $email = $_POST['email'];
-    $parola = $_POST['parola'];
-    $parola2 = $_POST['parola2'];
-    $sehir = $_POST['sehir'];
-    $cinsiyet = $_POST['cinsiyet'];
-    $hobiler = $_POST['hobi'];
-    $tanitim = $_POST['tanitim'];
+        /*echo "<pre>";
+        print_r($_POST);*/
+        
+        //empty(), içerisi boş mu değil mi kontrolü yapar
+        if(empty($_POST['adsoyad'])){
+            $adsoyadErr = "adsoyad zorunlu alan."."<br>";
+        }else{
+            $adsoyad = control_input($_POST['adsoyad']);
+        }
+
+        if(empty($_POST['email'])){
+            $emailErr = "email zorunlu alan."."<br>";
+        }else{
+            $email = $_POST['email'];
+        }
+
+        if(empty($_POST['parola'])){
+            $parolaErr =  "parola zorunlu alan."."<br>";
+        }else{
+            $parola = $_POST['parola'];
+        }
+
+        if(empty($_POST['parola2'])){
+            $parola2Err = "parola tekrarı zorunlu alan."."<br>";
+        }else{
+            $parola2 = $_POST['parola2'];
+        }
+
+        if($parola != $parola2){
+            $parola2Err = "parola bilgileri eşleşmiyor."."<br>";
+        }
+
+        if(empty($_POST['sehir'])){
+            $sehirErr = "sehir zorunlu alan."."<br>";
+        }else{
+            $sehir = $_POST['sehir'];
+        }
+    
+        if(empty($_POST['cinsiyet'])){
+            $cinsiyetErr = "cinsiyet zorunlu alan."."<br>";
+        }else{
+            $cinsiyet = $_POST['cinsiyet'];
+        }
+    
+        if(empty($_POST['tanitim'])){
+            $tanitimErr = "tanitim zorunlu alan."."<br>";
+        }else{
+            $tanitim = $_POST['tanitim'];
+        }
+    
+        //isset() içine yazılan değer tanımlandı mı kontrol eder
+        if(!isset($_POST['hobi'])){ // $_POST['hobi'] tanımlanmadıysa burası çalışır
+            $hobilerErr = "hobi zorunlu alan."."<br>";
+        }else{
+            $hobiler = $_POST['hobi'];
+        }
+    
 
     echo $adsoyad."<br><br>";
     echo $email."<br><br>";
@@ -31,41 +102,67 @@
 
     echo $tanitim."<br><br>";
 
-    echo "_______________________________________________________________<br><br>";
-
     /*
-    echo "<pre>";
-    print_r($_POST);
-    echo "</pre>";
-    */
+        echo "<pre>";
+        print_r($_POST);
+        echo "</pre>";
+        */
+
+    echo "_______________________________________________________________<br><br>";
 
     }
 
 ?>
+
+<style>
+    .error{
+        color: red;
+    }
+</style>
     
     
 <form action="register.php" method="POST">
-    adınız: <input type="text" name="adsoyad"> <br><br>
-    e-mail: <input type="text" name="email"> <br><br>
-    parola: <input type="text" name="parola"> <br><br>
-    parola tekrarı: <input type="text" name="parola2"> <br><br>
+
+    <!-- POST ile verileri gönderdikten sonra kalıcılığını sağlamak için neler yapabiliri?:
+            value="<?php //echo $adsoyad; ?>" 
+            <?php //if($sehir == '41') echo "selected" ?>
+            <?php //if($cinsiyet == 'erkek') echo "checked" ?>
+            <?php //if(isset($hobiler) && in_array('sinema', $hobiler)) echo "checked" ?>
+     -->
+    adınız: <input type="text" name="adsoyad" value="<?php echo $adsoyad; ?>"> 
+    <span class="error"><?php echo $adsoyadErr; ?><br><br></span>
+
+    e-mail: <input type="text" name="email" value="<?php echo $email; ?>"> 
+    <?php echo $emailErr; ?><br><br>
+
+    parola: <input type="text" name="parola"> 
+    <?php echo $parolaErr; ?><br><br>
+
+    parola tekrarı: <input type="text" name="parola2"> 
+    <?php echo $parola2Err; ?><br><br>
+
     şehir: <select name="sehir"> 
-        <option value="41">Kocaeli</option>
-        <option value="34">İstanbul</option>
-        <option value="20">Denizli</option>
-    </select><br><br>
-    cinsiyet: <input type="radio" name="cinsiyet" value="erkek">Erkek  
-              <input type="radio" name="cinsiyet" value="kadin">Kadın<br><br>
+        <option value="">Seçiniz</option>
+        <option value="41" <?php if($sehir == '41') echo "selected" ?>>Kocaelli</option>
+        <option value="34" <?php if($sehir == '34') echo "selected" ?>>İstanbul</option>
+        <option value="20" <?php if($sehir == '20') echo "selected" ?>>Denizli</option>
+    </select> 
+    <?php echo $sehirErr; ?><br><br>
+
+    cinsiyet: <input type="radio" name="cinsiyet" value="erkek" <?php if($cinsiyet == 'erkek') echo "checked" ?>>Erkek  
+              <input type="radio" name="cinsiyet" value="kadin" <?php if($cinsiyet == 'kadin') echo "checked" ?>>Kadın
+              <br><?php echo $cinsiyetErr; ?><br><br>
 
     <!--çoklu seçimleri POST methodu ile dizi şeklinde yakalamamız gerekiyor-->
-    hobileriniz: <input type="checkbox" name="hobi[]" value="sinema">Sinema 
-                 <input type="checkbox" name="hobi[]" value="spor">Spor
-                 <input type="checkbox" name="hobi[]" value="kitap okumak">Kitap Okumak
+    hobileriniz: <input type="checkbox" name="hobi[]" value="sinema" <?php if(isset($hobiler) && in_array('sinema', $hobiler)) echo "checked" ?>>Sinema 
+                 <input type="checkbox" name="hobi[]" value="spor" <?php if(isset($hobiler) && in_array('spor', $hobiler)) echo "checked" ?>>Spor
+                 <input type="checkbox" name="hobi[]" value="kitap okumak" <?php if(isset($hobiler) && in_array('kitap okumak', $hobiler)) echo "checked" ?>>Kitap Okumak
                  
-                 <br><br>
+                 <br><?php echo $hobilerErr; ?><br><br>
 
     kendinizi tanıtın: <br>
-        <textarea name="tanitim"></textarea> <br><br>
+        <textarea name="tanitim"><?php echo $tanitim; ?></textarea>
+         <?php echo $tanitimErr; ?><br><br>
 
     <input type="submit" value="Kayıt Ol">
 
@@ -74,3 +171,6 @@
 
         
 </form>
+
+
+
